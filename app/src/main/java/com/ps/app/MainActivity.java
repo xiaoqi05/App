8d46beb009b4e253c0a66f3ab20387454a04a7b2
xiaoqi05 <772508341@qq.com>
2016-04-25 15:11:11 +0800

    first step baidu map

--- app/src/main/java/com/ps/app/MainActivity.java
+++ app/src/main/java/com/ps/app/MainActivity.java
@@ -3,11 +3,35 @@ package com.ps.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 
+public class MainActivity extends AppCompatActivity {
+    private  MapView mMapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        //获取地图控件引用
+         mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
     }
 }
