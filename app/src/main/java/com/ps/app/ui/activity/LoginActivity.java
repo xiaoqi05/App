eca1a57b7060637a74f8a7bca208c110853e436e
xiaoqi05 <772508341@qq.com>
2016-04-29 16:28:34 +0800

    add mob

--- app/src/main/java/com/ps/app/ui/activity/LoginActivity.java
+++ app/src/main/java/com/ps/app/ui/activity/LoginActivity.java
@@ -3,14 +3,11 @@ package com.ps.app.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.ps.app.R;
-import com.ps.app.support.view.Code;
 
 public class LoginActivity extends BaseActivity implements View.OnClickListener {
 
-    private ImageView iv_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +20,10 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     }
 
     private void initData() {
-        iv_image.setImageBitmap(Code.getInstance().createBitmap());
-        System.out.println(Code.getInstance().getCode()+">>>>>>>>>>>>>");
+
     }
 
     private void findView() {
-        iv_image = (ImageView) findViewById(R.id.iv_image);
-        assert iv_image != null;
-        iv_image.setOnClickListener(this);
         findViewById(R.id.bt_login).setOnClickListener(this);
         findViewById(R.id.bt_register).setOnClickListener(this);
     }
@@ -39,10 +32,6 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.iv_image:
-                iv_image.setImageBitmap(Code.getInstance().createBitmap());
-                System.out.println(Code.getInstance().getCode()+">>>>>>>>>>>>>");
-                break;
             case R.id.bt_login:
                 break;
             case R.id.bt_register:
