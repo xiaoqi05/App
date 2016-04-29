b7d67bb39caf19eb17d96f526e0cfaa45b048101
xiaoqi05 <772508341@qq.com>
2016-04-29 15:09:37 +0800

    add splash

--- app/src/main/java/com/ps/app/ui/activity/BaseActivity.java
+++ app/src/main/java/com/ps/app/ui/activity/BaseActivity.java
@@ -1,10 +1,18 @@
 package com.ps.app.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.ps.app.R;
@@ -91,6 +99,43 @@ public class BaseActivity extends AppCompatActivity {
         actionBar.setDisplayShowHomeEnabled(true);
         if (!TextUtils.isEmpty(title)) {
             actionBar.setTitle(title);
+        }
+    }
+
+
+    public void showProgress(final ProgressBar mProgressView, final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    /**
+     * 获取 SharedPreferences
+     * @param config 值为空时使用默认值 config
+     * @return
+     */
+    public SharedPreferences getSharePreference(String config) {
+        if (TextUtils.isEmpty(config)) {
+            return getSharedPreferences("config", MODE_PRIVATE);
+        } else {
+            return getSharedPreferences(config, MODE_PRIVATE);
         }
     }
 
---
+++ app/src/main/java/com/ps/app/ui/activity/Splash.java
@@ -0,0 +1,72 @@
+package com.ps.app.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ProgressBar;
+
+import com.ps.app.R;
+
+import java.lang.ref.WeakReference;
+
+public class Splash extends BaseActivity {
+    ProgressBar progressBar;
+    public static final int DISMISS_PROGRESSBAR = 1;
+    private MyHandler myHandler = new MyHandler(this);
+
+    private static class MyHandler extends Handler {
+        private WeakReference<Splash> activityWeakReference;
+
+        public MyHandler(Splash activity) {
+            activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            Splash activity = activityWeakReference.get();
+            if (activity != null) {
+                switch (msg.what) {
+                    case DISMISS_PROGRESSBAR:
+                        activity.showProgress(activity.progressBar, false);
+                        if (activity.getSharePreference("").getBoolean("isLogin", true)) {
+                            activity.startActivity(new Intent(activity, MainActivity.class));
+                        } else {
+                            activity.startActivity(new Intent(activity, LoginActivity.class));
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        findView();
+        initData();
+    }
+
+    private void initData() {
+        showProgress(progressBar, true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    myHandler.sendEmptyMessage(DISMISS_PROGRESSBAR);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private void findView() {
+        progressBar = (ProgressBar) findViewById(R.id.splash_progress);
+    }
+
+
+}
