eba89f50acf3304e730a9f41dbb90adbddf1a0c0
xiaoqi05 <772508341@qq.com>
2016-05-03 13:34:33 +0800

    add MessageActivity actionbar

--- app/src/main/java/com/ps/app/ui/activity/MessageActivity.java
+++ app/src/main/java/com/ps/app/ui/activity/MessageActivity.java
@@ -1,15 +1,41 @@
 package com.ps.app.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.ps.app.R;
 
-public class MessageActivity extends AppCompatActivity {
+public class MessageActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        initActionBar(-1, "消息提醒");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_message, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.clear_message:
+                showShortToast("清空消息");
+                return true;
+            case R.id.marker_already_read:
+                showShortToast("标记为已读");
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
