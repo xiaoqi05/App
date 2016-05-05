baae7078409daa160d5f329f7dfd2db94ae180dd
xiaoqi05 <772508341@qq.com>
2016-05-05 14:10:11 +0800

    add location feature when open RouterActivity

--- app/src/main/java/com/ps/app/ui/activity/RouterActivity.java
+++ app/src/main/java/com/ps/app/ui/activity/RouterActivity.java
@@ -4,7 +4,17 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.ps.app.R;
+import com.ps.app.base.MyApplication;
+import com.ps.app.service.LocationService;
 import com.rey.material.app.DatePickerDialog;
 import com.rey.material.app.Dialog;
 import com.rey.material.app.DialogFragment;
@@ -16,6 +26,36 @@ public class RouterActivity extends BaseActivity implements View.OnClickListener
     private Button bt_get_start_time;
     private Button bt_get_end_time;
     private Button bt_search;
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private LocationService mLocationService;
+
+    private boolean isFirstLoc = true;
+    private BDLocationListener mListener = new BDLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //showShortToast(location.getAddrStr());
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +63,32 @@ public class RouterActivity extends BaseActivity implements View.OnClickListener
         setContentView(R.layout.activity_router);
         initActionBar(-1, "活动轨迹");
         findView();
+        initData();
+    }
+
+    private void initData() {
+        mLocationService = ((MyApplication) getApplicationContext()).locationService;
+        mLocationService.registerListener(mListener);
+        mLocationService.start();
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
     }
 
     private void findView() {
         bt_get_start_time = (Button) findViewById(R.id.bt_get_start_date);
         assert bt_get_start_time != null;
         bt_get_start_time.setOnClickListener(this);
-        
+
         bt_get_end_time = (Button) findViewById(R.id.bt_get_end_date);
         assert bt_get_end_time != null;
         bt_get_end_time.setOnClickListener(this);
-        
+
         bt_search = (Button) findViewById(R.id.bt_date_search);
         assert bt_search != null;
         bt_search.setOnClickListener(this);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        mMapView = (MapView) findViewById(R.id.mv_baidu_map);
     }
-
 
     @Override
     public void onClick(View v) {
@@ -104,4 +145,35 @@ public class RouterActivity extends BaseActivity implements View.OnClickListener
 
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+        mLocationService.unregisterListener(mListener);
+        mLocationService.stop();
+    }
+
 }
